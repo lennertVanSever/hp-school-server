945fb3691c453e350c8b940844003b1fd4817c7e
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-02-10 22:41:53 +0100

    removed logging

--- src/data/connection.js
+++ src/data/connection.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import mysql from 'mysql';
 dotenv.config();
-console.log(process.env);
+
 var connection = mysql.createConnection({
   host: 'hp-education.c7tfzswraxss.eu-central-1.rds.amazonaws.com',
   user: 'sara',
